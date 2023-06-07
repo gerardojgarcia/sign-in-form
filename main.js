f8332b29589f0ea3814363b86646ca9b584783c4
Gerardo Garcia <gerardojgarcia@pm.me>
2023-06-07 16:42:11 -0700

    added more inputs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+/*form data*/
 const firstName = document.querySelector('.firtname')
 const lastName = document.querySelector('.lastname')
+const email = document.querySelector('.email')
 const street = document.querySelector('.street')
 const state = document.querySelector('.state')
 const zipCode = document.querySelector('.zipcode')
